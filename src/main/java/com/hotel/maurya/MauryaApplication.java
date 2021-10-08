56f0793a5c79c0684ac6db7408673f388ee5809c
Anubhav Sharma <anubhav8804602148@gmail.com>
2021-10-08 14:14:56 +0530

    Started project with springboot.io. Maurya Hotel Application

---
+++ src/main/java/com/hotel/maurya/MauryaApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.maurya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MauryaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MauryaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/maurya/MauryaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.maurya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MauryaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
